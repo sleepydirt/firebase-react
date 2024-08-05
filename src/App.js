eca2b5db3bcad3fe902eb35fb4b60e50d014be06
aaron <aaronsoh01@gmail.com>
2024-08-05 15:28:18 +0800

    Install dependencies

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./views/LoginPage";
+import PostPageHome from "./views/PostPageHome";
+import SignUpPage from "./views/SignUpPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PostPageHome />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
