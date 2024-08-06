5a8d0efb1a390a9026ef48bb56bb8589672d8e75
aaron <aaronsoh01@gmail.com>
2024-08-06 16:32:12 +0800

    Added style to pages

--- src/views/LoginPage.js
+++ src/views/LoginPage.js
@@ -11,57 +11,71 @@ export default function LoginPage() {
   const navigate = useNavigate();
 
   return (
-    <Container
+    <div
       style={{
-        maxWidth: "600px",
-        border: "1px solid #fff",
-        borderRadius: "15px",
-        padding: "0.5rem 2rem",
-        margin: "30vh auto",
-        boxShadow:
-          "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+        background:
+          "linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)",
       }}
+      className="d-flex flex-column justify-content-center w-100 h-100"
     >
-      <h1 className="my-3">Login to your account</h1>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <a href="/signup">New? Sign up for an account</a>
-        </Form.Group>
-        <Button
-          variant="primary"
-          onClick={async (e) => {
-            setError("");
-            const canLogin = username && password;
-            if (canLogin)
-              try {
-                await signInWithEmailAndPassword(auth, username, password);
-                navigate("/");
-              } catch (error) {
-                setError(error.message);
-              }
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Container
+          style={{
+            background: "white",
+            width: "100%",
+            maxWidth: "600px",
+            border: "1px solid #fff",
+            borderRadius: "15px",
+            padding: "1.5rem",
+            boxShadow:
+              "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
           }}
         >
-          Login
-        </Button>
-      </Form>
-      <p>{error}</p>
-    </Container>
+          <h1 className="my-3">Login to your account</h1>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <a href="/signup">New? Sign up for an account</a>
+            </Form.Group>
+            <Button
+              variant="primary"
+              onClick={async (e) => {
+                setError("");
+                const canLogin = username && password;
+                if (canLogin)
+                  try {
+                    await signInWithEmailAndPassword(auth, username, password);
+                    navigate("/");
+                  } catch (error) {
+                    setError(error.message);
+                  }
+              }}
+            >
+              Login
+            </Button>
+          </Form>
+          <p>{error}</p>
+        </Container>
+      </div>
+    </div>
   );
 }
--- src/views/SignUpPage.js
+++ src/views/SignUpPage.js
@@ -12,81 +12,95 @@ export default function SignUpPage() {
   const [confirmPassword, setConfirmPassword] = useState("");
 
   return (
-    <Container
+    <div
       style={{
-        maxWidth: "600px",
-        border: "1px solid #fff",
-        borderRadius: "15px",
-        padding: "0.5rem 2rem",
-        margin: "30vh auto",
-        boxShadow:
-          "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+        background:
+          "linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)",
       }}
+      className="d-flex flex-column justify-content-center w-100 h-100"
     >
-      <h1 className="my-3">Sign up for an account</h1>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          <a href="/login">Have an existing account? Login here.</a>
-        </Form.Group>
-
-        <Button
-          variant="primary"
-          onClick={async (e) => {
-            setError("");
-            const canSignup = username && password;
-            if (canSignup) {
-              if (password === confirmPassword) {
-                try {
-                  await createUserWithEmailAndPassword(
-                    auth,
-                    username,
-                    password
-                  );
-                  navigate("/");
-                } catch (error) {
-                  setError(error.message);
-                }
-              } else {
-                setError(`Passwords do not match!`);
-              }
-            } else {
-              setError(`Invalid email and/or password!`);
-            }
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Container
+          style={{
+            background: "white",
+            width: "100%",
+            maxWidth: "600px",
+            border: "1px solid #fff",
+            borderRadius: "15px",
+            padding: "1.5rem",
+            boxShadow:
+              "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
           }}
         >
-          Sign Up
-        </Button>
-      </Form>
-      <p>{error}</p>
-    </Container>
+          <h1 className="my-3">Sign up for an account</h1>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <a href="/login">Have an existing account? Login here.</a>
+            </Form.Group>
+
+            <Button
+              variant="primary"
+              onClick={async (e) => {
+                setError("");
+                const canSignup = username && password;
+                if (canSignup) {
+                  if (password === confirmPassword) {
+                    try {
+                      await createUserWithEmailAndPassword(
+                        auth,
+                        username,
+                        password
+                      );
+                      navigate("/");
+                    } catch (error) {
+                      setError(error.message);
+                    }
+                  } else {
+                    setError(`Passwords do not match!`);
+                  }
+                } else {
+                  setError(`Invalid email and/or password!`);
+                }
+              }}
+            >
+              Sign Up
+            </Button>
+          </Form>
+          <p>{error}</p>
+        </Container>
+      </div>
+    </div>
   );
 }
